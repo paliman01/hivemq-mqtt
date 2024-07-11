d3c6692d4bd2daa22c095b89c0a108bacd294fb1
paliman01 <163369665+paliman01@users.noreply.github.com>
2024-07-11 20:34:42 +0800

    Add files via upload

---
+++ api.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+import paho.mqtt.publish as publish
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    mqtt_topic = request.args.get('mqtt_topic', '')
+    mqtt_payload = request.args.get('mqtt_payload', '')
+    publish.single(mqtt_topic, mqtt_payload, hostname="broker.hivemq.com")
+    return f'MQTT Topic: {mqtt_topic}, MQTT Payload: {mqtt_payload}. Message sent.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
